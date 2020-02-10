d22c3ebdd4f473058cee87e2693fd7f6c4865c19
methylDragon <methylDragon@gmail.com>
2020-02-10 03:33:15 -0800

    Update manifest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('PyPI/HISTORY.md') as history_file:
     HISTORY = history_file.read()
 
 setup_args = {'name': "onemapsg",
-              'version': "1.0.1",
+              'version': "1.0.1.post1",
               'description': "Python Wrapper Client for the OneMap API with automatic token handling",
               'long_description_content_type': "text/markdown",
               'long_description': README + '\n\n' + HISTORY,
@@ -20,4 +20,4 @@ setup_args = {'name': "onemapsg",
 install_requires = ['requests>=2.20.0', 'requests_toolbelt']
 
 if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
+    setup(**setup_args, install_requires=install_requires, include_package_data=True)
